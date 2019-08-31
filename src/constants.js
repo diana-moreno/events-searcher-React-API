3ffb30a7c55db1bc24ea4893b41432a076d71c84
Diana <d7@hotmail.es>
2019-08-31 20:15:06 +0200

    add API data in constants.js

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const BASE_API_URL = 'https://www.eventbriteapi.com/v3/'
+const API_TOKEN = 'H3XNKDXWLEAP3RPVC4X6'
+const sort_by = 'date'
+
+export const API_URLS = {
+  categories: `${BASE_API_URL}categories/?token=${API_TOKEN}`,
+  events: (search) => `${BASE_API_URL}events/search/?q=${search.name}&categories=${search.category}&sort_by=${sort_by}&token=${API_TOKEN}`
+}
