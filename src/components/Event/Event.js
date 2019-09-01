1ddd837468ff1ed1cc8b267d7d70cbfce3764e5c
Diana <d7@hotmail.es>
2019-09-01 19:57:47 +0200

    add a message when there isn't description informacion

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Event = ({event}) => { //event viene de props
-  let {text} = event.description;
-  if(text) {
-    text.length > 250 ? text = text.substring(0, 230).concat('...') : text = null; //limitar a 250 carácteres.
+
+  function validateTextDescription() {
+    let {text} = event.description;
+    if(text) {
+      text.length > 250 ? text = text.substring(0, 230).concat('...') : text = null; //limitar a 250 carácteres.
+    }
+    if(!text || text.length === 0) {
+      text = 'No description information.'
+    }
+    return text
   }
+
   function convertDateFormat(stringStart, stringEnd) {
     const dateStart = stringStart.slice(0, 10).split('-').reverse().join('-');
     const dateEnd = stringEnd.slice(0, 10).split('-').reverse().join('-');
@@ -20,7 +28,7 @@ const Event = ({event}) => { //event viene de props
 
         <div className='uk-card-body uk-flex uk-flex-column uk-flex-middle uk-flex-between'>
           <h3 className='uk-card-title'>{event.name.text}</h3>
-          <p>{text}</p>
+          <p>{validateTextDescription()}</p>
           <div>
             <p className='uk-text-bold'>{convertDateFormat(event.start.local, event.end.local)}</p>
             <p className='uk-text-bold'>Location: {event.venue.address.city}, {event.venue.address.country}</p>
