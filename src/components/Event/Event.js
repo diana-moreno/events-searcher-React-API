9518b81812efa1a3ada801f3e9ba677f277174ed
Diana <d7@hotmail.es>
2019-09-01 20:37:57 +0200

    the icon changes when the user clicks to add to favorites

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Event = ({event}) => { //event viene de props
+class Event extends Component { //event viene de props
+  state = {
+    favorite: false
+  }
 
-  function validateTextDescription() {
-    let {text} = event.description;
+  validateTextDescription() {
+    let {text} = this.props.event.description;
     if(text) {
       text.length > 250 ? text = text.substring(0, 230).concat('...') : text = null; //limitar a 250 carácteres.
     }
@@ -13,36 +16,49 @@ const Event = ({event}) => { //event viene de props
     return text
   }
 
-  function convertDateFormat(stringStart, stringEnd) {
+  convertDateFormat(stringStart, stringEnd) {
     const dateStart = stringStart.slice(0, 10).split('-').reverse().join('-');
     const dateEnd = stringEnd.slice(0, 10).split('-').reverse().join('-');
     return dateStart === dateEnd ? <span>Date: {dateStart}</span> : <span>Date: from {dateStart} to {dateEnd}</span>
    }
 
-  return (
-    <div className='center'>
-      <div className='uk-card-small uk-card-default'>
-        <div className='uk-card-media-top uk-flex uk-flex-center'>
-          {event.logo ? <img src={event.logo.url} alt={event.name} /> : null}
-        </div>
+   addToFavorites = () => {
+    this.setState({
+      favorite: true
+    })
+   }
 
-        <div className='uk-card-body uk-flex uk-flex-column uk-flex-middle uk-flex-between'>
-          <h3 className='uk-card-title'>{event.name.text}</h3>
-          <p>{validateTextDescription()}</p>
-          <div>
-            <p className='uk-text-bold'>{convertDateFormat(event.start.local, event.end.local)}</p>
-            <p className='uk-text-bold'>Location: {event.venue.address.city}, {event.venue.address.country}</p>
+  render() {
+    return (
+      <div className='center'>
+        <div className='uk-card-small uk-card-default'>
+          <i className={this.state.favorite
+                          ? 'far fa-heart uk-position-absolute'
+                          : "fas fa-plus uk-position-absolute"}
+             title='Add to favorites'
+             onClick={this.addToFavorites}></i>
+          <div className='uk-card-media-top uk-flex uk-flex-center'>
+            {this.props.event.logo ? <img src={this.props.event.logo.url} alt={this.props.event.name} /> : null}
+          </div>
+
+          <div className='uk-card-body uk-flex uk-flex-column uk-flex-middle uk-flex-between'>
+            <h3 className='uk-card-title'>{this.props.event.name.text}</h3>
+            <p>{this.validateTextDescription()}</p>
+            <div>
+              <p className='uk-text-bold'>{this.convertDateFormat(this.props.event.start.local, this.props.event.end.local)}</p>
+              <p className='uk-text-bold'>Location: {this.props.event.venue.address.city}, {this.props.event.venue.address.country}</p>
+            </div>
+          </div>
+
+          <div className='uk-card-footer'>
+            <a target='_blank' rel='noopener noreferrer' href={this.props.event.url} className='uk-button uk-button-secondary'>
+              More information
+            </a>
           </div>
         </div>
-
-        <div className='uk-card-footer'>
-          <a target='_blank' rel='noopener noreferrer' href={event.url} className='uk-button uk-button-secondary'>
-            More information
-          </a>
-        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default Event;
